73bbe83517690e754ad1a639c3542575af7586ed
frankosgit <132131624+frankosgit@users.noreply.github.com>
2024-08-22 12:59:52 +0200

    init

---
+++ src/main/java/com/FrancisJones/NextWorkout/NextWorkout.java
@@ -0,0 +1,13 @@
+package com.FrancisJones.NextWorkout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextWorkout {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NextWorkout.class, args);
+    }
+
+}
---
+++ src/test/java/com/FrancisJones/NextWorkout/NextWorkoutTests.java
@@ -0,0 +1,13 @@
+package com.FrancisJones.NextWorkout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextWorkoutTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
